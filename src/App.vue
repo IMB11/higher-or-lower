0b58bd1833a8c73a6844f6b1e8853e308211d256
Calum <contact@mineblock11.dev>
2023-05-25 18:50:17 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Game from "./components/Game.vue";
+
+export default defineComponent({
+  components: {
+    Game,
+  },
+});
+</script>
+
+<template>
+  <h1>Higher or Lower</h1>
+  <Game />
+</template>
+
+<style scoped></style>
